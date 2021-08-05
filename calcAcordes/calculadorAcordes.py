e56845816c87c95c7c4dc43c0c9847a8870ee123
toti <sam17332@uvg.edu.gt>
2021-08-04 20:48:11 -0600

    Se agrega funcionalidad de mostrar el tipo del acorde.

--- calcAcordes/calculadorAcordes.py
+++ calcAcordes/calculadorAcordes.py
@@ -14,8 +14,10 @@ print('''
     | fa# | sol | sol#| la  | la# | si  |
     -------------------------------------
 ''')
+
 nota = input("Ingrese una de las notas de arriba para saber su escala y su acorde: ")
 print()
+
 if (nota in notas):
     indexActual = notas.index(nota)
     for i in sumasEscala:
@@ -29,20 +31,39 @@ if (nota in notas):
     print("La escala de " + nota + " es: " + escala)
     print()
 
-    if("#" not in nota):
-        cantidad = len(arrayEscala)
-        for i in range(cantidad):
-            arrayEscala.append(arrayEscala[i])
+    cantidad = len(arrayEscala)
+    for i in range(cantidad):
+        arrayEscala.append(arrayEscala[i])
 
-        for i in range(cantidad):
-            arrayAcorde = []
-            arrayAcorde.append(arrayEscala[i])
-            notaActual = arrayEscala.index(arrayEscala[i])
-            for j in sumasAcorde:
-                notaActual += j
-                arrayAcorde.append(arrayEscala[notaActual])
-            print("El acorde # " + str(cont) + " es: " + str(arrayAcorde))
-            cont += 1
+    for i in range(cantidad):
+        arrayAcorde = []
+        arrayAcorde.append(arrayEscala[i])
+        notaActual = arrayEscala.index(arrayEscala[i])
+        for j in sumasAcorde:
+            notaActual += j
+            arrayAcorde.append(arrayEscala[notaActual])
+        index0 = notas.index(arrayAcorde[0])
+        index1 = notas.index(arrayAcorde[1])
+        index2 = notas.index(arrayAcorde[2])
+        salto1 = index1 - index0
+        if(index0 > index1):
+            salto1 = (index1 + 12) - index0
+        salto2 = index2 - index1
+        if(index1 > index2):
+            salto2 = (index2 + 12) - index1
+
+        tipo = ""
+        if(salto1 == 3 and salto2 == 3):
+            tipo = "Disminuido"
+        elif(salto1 == 4 and salto2 == 4):
+            tipo = "Aumentado"
+        elif(salto1 == 3 and salto2 == 4):
+            tipo = "Menor"
+        elif(salto1 == 4 and salto2 == 3):
+            tipo = "Mayor"
+
+        print("El acorde # " + str(cont) + " es: " + str(arrayAcorde) + ". Su tipo es: " + tipo)
+        cont += 1
 
 else:
     print("Ingrese una nota valida!!")
